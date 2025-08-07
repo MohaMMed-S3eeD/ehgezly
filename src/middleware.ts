f169d8818f4b0369d7665f13b8626fda5981ad3e
Mohamed Seed <mosaeed7888@gmail.com>
2025-08-07 18:01:57 +0300

    Refactor authentication middleware to streamline session management by replacing NextAuth with direct auth function calls, improving redirect logic for public and protected routes.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,16 @@
 import { auth } from "@/auth"
 import { NextResponse } from "next/server";
-import authConfig from "./auth.config"
-import NextAuth from "next-auth";
 
-export const { auth: middleware } = NextAuth(authConfig)
-export default middleware(async (req) => {
+export default auth(async (req) => {
     const { nextUrl } = req;
     const path = nextUrl.pathname;
     const isPublic = path === "/login" || path === "/register";
-    const session = await auth();
-    if (!session && !isPublic) {
+    
+    if (!req.auth && !isPublic) {
         return NextResponse.redirect(new URL("/login", req.url));
     }
-    if (session && isPublic) {
+    
+    if (req.auth && isPublic) {
         return NextResponse.redirect(new URL("/profile", req.url));
     }
 })
