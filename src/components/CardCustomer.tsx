6d6b98c1f64f067607395b9a37267da92546151b
Mohamed Seed <mosaeed7888@gmail.com>
2025-08-15 23:15:28 +0300

    Update CardCustomer component to include customer details, ratings, and testimonial for enhanced user engagement.

--- src/components/CardCustomer.tsx
+++ src/components/CardCustomer.tsx
@@ -3,7 +3,41 @@ import React from "react";
 
 const CardCustomer = () => {
   return (
-    <div></div>
+    <div>
+      <div>
+        <div>
+          <Image
+            src="/images/customer1.png"
+            alt=""
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+          <h3>John Doe</h3>
+        </div>
+        <div>
+          <p className="flex gap-1 ">
+            <span className="text-white px-1 bg-[#269C54]">★</span>
+            <span className="text-white px-1 bg-[#269C54]">★</span>
+            <span className="text-white px-1 bg-[#269C54]">★</span>
+            <span className="text-white px-1 bg-[#269C54]">★</span>
+            <span className="text-white px-1  bg-gradient-to-r from-[#269C54] from-50% to-gray-500 to-50% ">
+              ★
+            </span>
+          </p>
+        </div>
+        <p>
+          &quot;I was struggling to find a reliable and affordable moving
+          company until I found this one. They were professional, efficient, and
+          made the entire process stress-free. I highly recommend them!&quot;
+        </p>
+      </div>
+      <div className="">
+        <div className=" text-center text-gray-500 border-2 border-gray-300 rounded-lg p-4">
+          Plaming
+        </div>
+      </div>
+    </div>
   );
 };
 
