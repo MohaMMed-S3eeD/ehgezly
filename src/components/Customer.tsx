a9ba1e3c1f3ea7aad4aa3bf6eba6a7db82a78208
Mohamed Seed <mosaeed7888@gmail.com>
2025-08-17 22:42:11 +0300

    Update the Customer component to add design improvements, including text formatting and element layout adjustments to increase the appeal of the user interface.

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -3,21 +3,21 @@ import CardCustomer from "./CardCustomer";
 
 const Customer = () => {
   return (
-    <section>
+    <section className=" min-h-screen m-4">
       <div
         id="Header"
-        className="flex flex-col gap-4 min-h-screen justify-center items-center text-center"
+        className="flex gap-4 justify-between items-center text-center "
       >
-        <div>
-          <h1>What Our Customers Are Saying</h1>
-          <p>
+        <div className="flex flex-2 flex-col gap-4 text-left">
+          <h1 className="text-2xl font-bold">What Our Customers Are Saying</h1>
+          <p className="text-gray-300 text-sm max-w-md">
             Discover why our clients love us! Read real testfnonials from
             satisfied customers who have experienced our top-notch services and
             see how we&apos;ve made their lives easier
           </p>
         </div>
-        <div>
-          <p>Excellent</p>
+        <div className="flex flex-1  gap-4">
+          <p className="flex-1">Excellent</p>
           <p className="flex gap-1 ">
             <span className="text-white px-1 bg-[#269C54]">★</span>
             <span className="text-white px-1 bg-[#269C54]">★</span>
@@ -27,7 +27,7 @@ const Customer = () => {
               ★
             </span>
           </p>
-          <p>Trustpilot</p>
+          <p className="flex-1">Trustpilot</p>
         </div>
       </div>
       <div id="Cards">
