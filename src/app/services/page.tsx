614f8a522e4a5dcbbbc4e2b4371d4ee98589e40f
Mohamed Seed <mosaeed7888@gmail.com>
2025-08-19 06:39:50 +0300

    Update services page layout to enhance responsiveness by adjusting grid columns for better display on various screen sizes.

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -39,7 +39,7 @@ export default async function Page() {
           )}
         </div>
 
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 ">
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4">
           {servicesRes.data?.map((service, index) => {
             const now = new Date();
             const providerName =
